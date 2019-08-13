479ecb144cd17bfc4ba1913e471ec21c199a3185
hhc <hc_huang@droidhang.com>
2019-08-13 14:52:42 +0800

    no message

---
+++ Test_2D_2/Assets/Lua/Scenes/Rest.lua
@@ -0,0 +1,15 @@
+
+--[[
+    负责Title场景的初始化
+--]]
+local PathMgr = require("PathManager")
+local ResourceMgr = require("ResourceManager")
+local SceneMgr = require("SceneManager")
+
+local Rest = {}
+
+function Rest:InitScene()
+    print("switch to Rest Scene")
+end
+
+return Rest
