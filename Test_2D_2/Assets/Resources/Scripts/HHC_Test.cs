4c9084fae3911cf00b2740c910e3025e0ff04679
hhc <hc_huang@droidhang.com>
2019-08-23 11:03:03 +0800

    nothing

---
+++ Test_2D_2/Assets/Resources/Scripts/HHC_Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HHC_Test : MonoBehaviour
+{
+    public Slider s;
+
+    public Text t;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        t.text = "hello";
+        s.value = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
